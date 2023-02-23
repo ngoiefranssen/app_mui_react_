e53a1a9b29af66595dfc614dd4c303922060e903
Franssen <ngoiefranssen04@gmail.com>
2023-02-23 11:58:00 +0200

    Wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,39 @@
 import {
   AppBar,
+  Badge,
   Grid,
+  IconButton,
+  InputBase,
   Toolbar,
 } from '@mui/material'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import ChatIcon from '@mui/icons-material/Chat';
 import React from 'react'
 
 const Header = () => {
   return (
     <AppBar position="static" color=''>
       <Toolbar>
-        <Grid contained></Grid>
+        <Grid container>
+          <Grid item></Grid>
+          <Grid item sm></Grid>
           <Grid item>
-            
+            <IconButton>
+              <Badge badgeContent={3} color="secondary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={7} color="primary">
+                <ChatIcon/>
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <PowerSettingsNewIcon />
+            </IconButton>
           </Grid>
-          <Grid>
-
-          </Grid>
-          <Grid></Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   )
